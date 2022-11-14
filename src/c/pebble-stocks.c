d2280686dba87458c7cfcacdf57fb60b38b934e3
Claudio <71095411+itsthered1@users.noreply.github.com>
2022-11-14 00:47:54 -0800

    Progress...
    - Switched APIs (AlphaVantage rate limit issues)
    - Wrote cell draw function, changes color based on pos/neg change
    - Began work on stock info screen

--- src/c/pebble-stocks.c
+++ src/c/pebble-stocks.c
@@ -1,57 +1,149 @@
 /*
   TO-DO BEFORE HACKATHON:
     - Add a "refresh" button to the main screen
-    - Change highlight colours based on positive/negative change
     - Display detailed stock data upon clicking a ticker
     - Figure out data being sent out of order (relative to array in index.js)
+    - Deal with AV API limitations, data coming in incorrectly upon second/third/etc. request
   TO-DO (NO DEADLINE):
     - Implement graphs for tickers
 */
-
 #include <pebble.h>
 #include <math.h>
 
+#define MAX_TICKER_LENGTH 5
+#define TICKER_CELL_MIN_HEIGHT PBL_IF_ROUND_ELSE(49, 45)
+
+typedef struct TickerItem {
+  // the name displayed for the item
+  char name[MAX_TICKER_LENGTH];
+} TickerItem;
+
 static Window *s_window;
+static Window *s_detail_window;
 static TextLayer *s_text_layer;
 static MenuLayer *s_menu_layer;
 static Layer *s_canvas_layer;
 
-static int received = 0;
-static int total = 0;
+static TickerItem *s_ticker_items[10];
 
-static char tickers[15][5]; 
+static int received = 0;
+static int total = -1;
+
+static int selected;
+
+static char tickers[15][MAX_TICKER_LENGTH]; 
 static char open[15][10]; 
 static char high[15][10]; 
 static char low[15][10]; 
-static char price[15][10]; 
-static char volume[15][10]; 
+static char price[15][10];
 static char close[15][10]; 
 static char change[15][10]; 
 static char changePercent[15][10]; 
+
+static void select_click_callback(MenuLayer *s_menu_layer, MenuIndex *cell_index, void *data) {
+  // Get which row
+  selected = cell_index->row;
+  window_stack_push(s_detail_window, true);
+}
+
+static void detail_window_load(Window *window) {
+  int id = total - (selected - 1) - 2;
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  TextLayer *ticker_label = text_layer_create(GRect(5, -3, bounds.size.w, bounds.size.h));
+  TextLayer *price_label = text_layer_create(GRect(0, 27, bounds.size.w, bounds.size.h));
+  TextLayer *change_label = text_layer_create(GRect(0, 50, bounds.size.w, bounds.size.h));
+
+  text_layer_set_text(ticker_label, tickers[id]);
+  text_layer_set_font(ticker_label, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_background_color(ticker_label, GColorBlack);
+  text_layer_set_text_color(ticker_label, GColorWhite);
+  text_layer_set_text_alignment(ticker_label, GTextAlignmentLeft);
+
+  text_layer_set_text(price_label, price[id]);
+  text_layer_set_font(price_label, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_background_color(price_label, GColorBlack);
+  text_layer_set_text_color(price_label, GColorWhite);
+  text_layer_set_text_alignment(price_label, GTextAlignmentLeft);
+
+  text_layer_set_text(change_label, change[id]);
+  text_layer_set_font(change_label, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_background_color(change_label, GColorBlack);
+  text_layer_set_text_color(change_label, GColorWhite);
+  text_layer_set_text_alignment(change_label, GTextAlignmentLeft);
+
+  layer_add_child(window_get_root_layer(s_detail_window), text_layer_get_layer(ticker_label));
+  // layer_add_child(window_get_root_layer(s_detail_window), text_layer_get_layer(price_label));
+  // layer_add_child(window_get_root_layer(s_detail_window), text_layer_get_layer(change_label));
+
+  window_set_background_color(s_detail_window, GColorBlack);
+}
 
 static uint16_t get_num_rows_callback(MenuLayer *s_menu_layer, uint16_t section_index, void *context) {
   return received;
 }
 
 static int16_t get_cell_height_callback(struct MenuLayer *menu_layer, MenuIndex *cell_index, void *context) {
-  return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
+  return TICKER_CELL_MIN_HEIGHT;
+}
+
+static void draw_ticker_cell(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index) {
+  int id = total - (cell_index->row - 1) - 2;
+
+  TickerItem *item = s_ticker_items[id];
+
+  GRect bounds = layer_get_bounds(cell_layer);
+  GRect text_bounds;
+  GTextAlignment alignment = GTextAlignmentLeft;
+
+  graphics_draw_rect(ctx, bounds);
+  if (atoi(change[id]) > 0) { //convert change to int and check if it's positive or negative
+    if(menu_cell_layer_is_highlighted(cell_layer)) { //if positive, set highlight color to green
+      graphics_context_set_fill_color(ctx, GColorGreen);
+    } else {
+      graphics_context_set_fill_color(ctx, GColorBlack);
+    }
+  } else if (atoi(change[id]) < 0) { //if negative, set highlight color to red
+    if(menu_cell_layer_is_highlighted(cell_layer)) {
+      graphics_context_set_fill_color(ctx, GColorRed);
+    } else {
+      graphics_context_set_fill_color(ctx, GColorBlack);
+    }
+  } else {
+    if(menu_cell_layer_is_highlighted(cell_layer)) { //if no change, set highlight color to gray
+      graphics_context_set_fill_color(ctx, GColorLightGray);
+    } else {
+      graphics_context_set_fill_color(ctx, GColorBlack);
+    } 
+  }
+
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+  if(bounds.size.h == TICKER_CELL_MIN_HEIGHT) {
+    menu_cell_basic_draw(ctx, cell_layer, tickers[id], changePercent[id], NULL);
+  }
+
+  if (menu_cell_layer_is_highlighted(cell_layer)) {
+    graphics_context_set_stroke_color(ctx, GColorWhite);
+  }
 }
 
 static void draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *context) {
-  menu_cell_basic_draw(ctx, cell_layer, tickers[cell_index->row], price[cell_index->row], NULL);
+  draw_ticker_cell(ctx, cell_layer, cell_index);
 }
 
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Message received!");
+  
   Tuple *symbol_tuple = dict_find(iter, MESSAGE_KEY_Symbol);
   Tuple *open_tuple = dict_find(iter, MESSAGE_KEY_Open);
   Tuple *high_tuple = dict_find(iter, MESSAGE_KEY_High);
   Tuple *low_tuple = dict_find(iter, MESSAGE_KEY_Low);
   Tuple *price_tuple = dict_find(iter, MESSAGE_KEY_Price);
-  Tuple *volume_tuple = dict_find(iter, MESSAGE_KEY_Volume);
   Tuple *close_tuple = dict_find(iter, MESSAGE_KEY_PrevClose);
   Tuple *change_tuple = dict_find(iter, MESSAGE_KEY_Change);
   Tuple *change_percent_tuple = dict_find(iter, MESSAGE_KEY_ChangePercent);
-  Tuple *ready_tuple = dict_find (iter, MESSAGE_KEY_Ready);
   Tuple *total_tuple = dict_find (iter, MESSAGE_KEY_TotalTickers);
 
   strcpy(tickers[received], symbol_tuple->value->cstring);
@@ -59,22 +151,21 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
   strcpy(high[received], high_tuple->value->cstring);
   strcpy(low[received], low_tuple->value->cstring);
   strcpy(price[received], price_tuple->value->cstring);
-  strcpy(volume[received], volume_tuple->value->cstring);
   strcpy(close[received], close_tuple->value->cstring);
   strcpy(change[received], change_tuple->value->cstring);
   strcpy(changePercent[received], change_percent_tuple->value->cstring);
   total = total_tuple->value->int32;
 
-  if (ready_tuple->value->int32 == 1) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Valid ticker data. Got ready tuple. %d", 1);
-    received++;
-  } else if (ready_tuple->value->int32 == 0) {
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Not valid ticker data %d", 1);
+  received++;
+  if (received > total) {
+    received = 1; //restart counter once new batch of data is received
   }
-
-  if (received == total) { //only reload once all data has been received
+  if (received == total) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Reloading menu layer");
     menu_layer_reload_data(s_menu_layer);
   }
+  
+  
 } 
 
 static void inbox_dropped_handler(AppMessageResult reason, void *context){
@@ -86,18 +177,35 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Loading text
+  // APP_LOG(APP_LOG_LEVEL_DEBUG, "Loading...");
+  // s_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  // text_layer_set_text(s_text_layer, "Loading...");
+  // text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  // text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  // layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_text_layer));
+  
   s_menu_layer = menu_layer_create(bounds);
   menu_layer_set_click_config_onto_window(s_menu_layer, window);
 #if defined(PBL_COLOR)
-  menu_layer_set_normal_colors(s_menu_layer, GColorWhite, GColorBlack);
-  menu_layer_set_highlight_colors(s_menu_layer, GColorGreen, GColorWhite);
+  menu_layer_set_normal_colors(s_menu_layer, GColorBlack, GColorWhite);
+  menu_layer_set_highlight_colors(s_menu_layer, GColorBlack, GColorWhite);
 #endif
+  s_detail_window = window_create();
+  window_set_window_handlers(s_detail_window, (WindowHandlers) {
+      .load = detail_window_load,
+  });
+
   menu_layer_set_callbacks(s_menu_layer, NULL, (MenuLayerCallbacks) {
       .get_num_rows = get_num_rows_callback,
       .draw_row = draw_row_callback,
       .get_cell_height = get_cell_height_callback,
-    });
+      .select_click = select_click_callback,
+  });
+  menu_layer_set_click_config_onto_window(s_menu_layer, window);
   layer_add_child(window_layer, menu_layer_get_layer(s_menu_layer));
+
+  // request_data();
 }
 
 static void window_unload() {
@@ -105,6 +213,7 @@ static void window_unload() {
   text_layer_destroy(s_text_layer);
   menu_layer_destroy(s_menu_layer);
   window_destroy(s_window);
+  window_destroy(s_detail_window);
 }
 
 static void init() {
@@ -118,8 +227,8 @@ static void init() {
     .load = window_load,
     .unload = window_unload,
   });
-  window_stack_push(s_window, true);
 
+  window_stack_push(s_window, true);
 }
 
 static void deinit() {
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,69 +1,75 @@
 const Clay = require('pebble-clay');
 const clayConfig = require('./config.json');
-const clay = new Clay(clayConfig);
+const clay = new Clay(clayConfig, null, { autoHandleEvents: false });
 const MessageQueue = require('message-queue-pebble');
 
-const tickers = ["AAPL", "MSFT", "AMZN", "META"]; //set default tickers in case user doesn't set any
-const totalTickers = tickers.length; //to be returned for menu layer construction
+let tickers = []; //set default tickers in case user doesn't set any
+let totalTickers; //to be returned for menu layer construction
 
-const key = "KOBYCP2KJDXZNUYP";
+const key = "cdond1aad3i3u5goj3agcdond1aad3i3u5goj3b0"
 
 Pebble.addEventListener('showConfiguration', function(e) {
     Pebble.openURL(clay.generateUrl());
 });
 
-Pebble.addEventListener('webviewclsoed', function(e) {
-
+Pebble.addEventListener('webviewclosed', function(e) {
+    if (e && !e.response) {
+        return;
+    }
+    let configuration = clay.getSettings(e.response, false);
+    localStorage.setItem("configuration", JSON.stringify(configuration));
+    let str = configuration.Tickers.value;
+    tickers = str.split(", ");
+    totalTickers = tickers.length;
+    //fetchWatchlist(); //figure out full refresh on config change
 });
 
 Pebble.addEventListener('ready', function () {
     console.log("Ready!");
-    fetch();
+    var configuration = JSON.parse(localStorage.getItem("configuration"));
+    if (configuration) {
+        let str = configuration.Tickers.value;
+        tickers = str.split(", ");
+        totalTickers = tickers.length;
+        fetchWatchlist();
+    }
 });
 
 Pebble.addEventListener('appmessage', function (e) {
     console.log("AppMessage received!");
 });
 
-
-
-function fetch() {
+function fetchWatchlist() {
     for (let i = 0; i < totalTickers; i++) {
         const ticker = tickers[i];
-        const url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + ticker + "&apikey=" + key;
+        const url = "https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + key;
         const req = new XMLHttpRequest();
         req.open('GET', url, true);
-        req.onload = function(e) {
+        req.onload = function (e) {
             if (req.status == 200) {
-                var data = JSON.parse(req.responseText);
+                const data = JSON.parse(req.responseText);
+                let message = {
+                    "Symbol": ticker,
+                    "Open": data["o"].toString(),
+                    "High": data["h"].toString(),
+                    "Low": data["l"].toString(),
+                    "Price": data["c"].toString(),
+                    "PrevClose": data["pc"].toString(),
+                    "Change": data["d"].toString(),
+                    "ChangePercent": data["dp"].toString(),
+                    "TotalTickers": totalTickers
+                };
 
-                if(data["Global Quote"]) {
-                    var quote = data["Global Quote"];
-
-                    var message = {
-                        "Symbol": quote["01. symbol"],
-                        "Open": quote["02. open"],
-                        "High": quote["03. high"],
-                        "Low": quote["04. low"],
-                        "Price": quote["05. price"],
-                        "Volume": quote["06. volume"],
-                        "PrevClose": quote["08. previous close"],
-                        "Change": quote["09. change"],
-                        "ChangePercent": quote["10. change percent"],
-                        "Ready": 1,
-                        "TotalTickers": totalTickers
-                    };
-
-                    MessageQueue.sendAppMessage(message, onSuccess, onFailure);
-                } else {
-
-                }
-            } else {
-                console.log("Error");
-            }
-        }
-        req.send();
+                MessageQueue.sendAppMessage(message, onSuccess, onFailure);
+            }     
+        } 
+        req.send();  
     }
+}
+
+function fetchDetails() {
+    //fetch details for selected stock
+    
 }
 
 function onSuccess(data){
